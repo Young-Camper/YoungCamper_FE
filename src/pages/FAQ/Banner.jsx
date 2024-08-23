0f0b43e839255662e6d1dd2031ba5c1209002dfd
ksinji <akstp1717@naver.com>
2024-08-23 20:16:14 +0900

    💄 style: 컴포넌트 수정

--- src/pages/FAQ/Banner.jsx
+++ src/pages/FAQ/Banner.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import { ContentWrapper } from "../../style/commonStyle";
 import * as S from "./style";
-import useMediaQueries from "../../hooks/useMediaQueries";
+import MainTitle from "../../components/ui/MainTitle";
 
-const Banner = props => {
-	const { isMobile, isTablet, isDesktop } = useMediaQueries();
-	
-  return (
-	<S.BannerWrapper>
-		<ContentWrapper>
-		<S.TitleWrapper isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}>
-			<S.MainTitle>FAQ</S.MainTitle>
-			<S.SubTitle>자주 물어보는 질문에 대한 답변을 정리했어요.</S.SubTitle>
-		</S.TitleWrapper>
-		</ContentWrapper>
-	</S.BannerWrapper>
-  )
-}
+const Banner = () => {
+	return (
+	  <>
+		<MainTitle title="FAQ" />
+	  </>
+	);
+  };
+  
 
 export default Banner
