7bac9f8b700b2b59d589e40091f8caece59a937e
Hyeona01 <sha0209@dgu.ac.kr>
2024-08-24 01:11:15 +0900

    ✨ feat: 타임테이블 연예인 모달 1차 구현 #6

--- src/pages/Performance/TimeTable.jsx
+++ src/pages/Performance/TimeTable.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import TitleSet from "../../components/ui/TitleSet";
+import artists from "../../data/artists.json";
+import ArtistModal from "./ArtistModal";
 
-const TimeTable = () => {
+const TimeTable = ({ modalOpen, setModalOpen }) => {
+  const [clickedArtist, setClickedArtist] = useState({});
+
+  const handleOpenModal = (artist) => {
+    setModalOpen(true);
+    setClickedArtist(artist);
+  };
+
   return (
-    <TitleSet
-      mainText="타임테이블"
-      subText="영캠프는 대한민국 대학 불교 동아리들이 연합하여 주최하는 특별한 축제입니다."
-    />
+    <>
+      {modalOpen && (
+        <ArtistModal setModalOpen={setModalOpen} artist={clickedArtist} />
+      )}
+      <TitleSet
+        mainText="타임테이블"
+        subText="영캠프는 대한민국 대학 불교 동아리들이 연합하여 주최하는 특별한 축제입니다."
+      />
+      <div style={{ display: "flex", gap: "30px" }}>
+        {artists &&
+          artists.map((artist) => (
+            <div key={artist.id}>
+              <img
+                src={artist.imgURL}
+                onClick={() => handleOpenModal(artist)}
+              />
+              <div>{artist.name}</div>
+              <div>{artist.time}</div>
+              <div>{artist.etc}</div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
