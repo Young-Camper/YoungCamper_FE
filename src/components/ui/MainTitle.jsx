076f798516b5aaedc56debe110ce82945a13a827
Hyeona01 <sha0209@dgu.ac.kr>
2024-08-23 16:16:01 +0900

    💄 style: 디자인 변경에 따른 수정사항 반영 #4

--- src/components/ui/MainTitle.jsx
+++ src/components/ui/MainTitle.jsx
@@ -1,25 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 import useMediaQueries from "../../hooks/useMediaQueries";
+import { ContentWrapper } from "../../style/commonStyle";
 
 const Wrapper = styled.div`
   display: flex;
   width: 100vw;
   height: ${(props) =>
-    props.isMobile ? "100px" : props.isTablet ? "180px" : "280px"};
-  padding: ${(props) => "0px 20px"};
+    props.isMobile
+      ? "100px" /* 모바일 미반영 */
+      : props.isTablet
+      ? "180px" /* 테블릿 미반영 */
+      : "280px"}; /* 데스크탑 280px */
   flex-direction: column;
   justify-content: center;
-  align-items: start;
+  align-items: flex-start;
+  gap: var(--sds-size-space-600);
+  align-self: stretch;
 
-  color: var(--text-real-black, #0a0b0a);
-  text-align: center;
-  font-family: "Noto Sans KR";
+  opacity: var(--sds-size-stroke-border);
+  background: var(--Base-Base-White, #fafafa);
   font-size: ${(props) =>
-    props.isMobile ? "20px" : props.isTablet ? "30px" : "40px"};
+    props.isMobile
+      ? "20px" /* 모바일 미반영 */
+      : props.isTablet
+      ? "30px" /* 테블릿 미반영 */
+      : "40px"}; /* 데스크탑 40px */
+`;
+const BannerWrapper = styled(ContentWrapper)`
+  padding: 0px 228px; /* 180px + 48px */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 24px;
+  align-self: stretch;
+`;
+const MainText = styled.div`
+  color: var(--new-main-black, #0a0b0a);
+  text-align: center;
+
+  /* Primary/Desktop/Hero2 */
+  font-family: Montserrat;
+  font-size: 38px;
   font-style: normal;
-  font-weight: 700;
-  line-height: 120%;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: -3.04px;
+`;
+const SubText = styled.div`
+  color: var(--new-grey-grey600, #92a5b5);
+
+  /* Primary/Desktop/Body2_regular */
+  font-family: Montserrat;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 120% */
+  letter-spacing: -0.1px;
 `;
 
 const MainTitle = ({ title }) => {
@@ -27,7 +66,14 @@ const MainTitle = ({ title }) => {
 
   return (
     <Wrapper isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}>
-      {title}
+      <BannerWrapper>
+        <MainText>{title}</MainText>
+        {/* 아래 글은 추후 props로 수정될 예정입니다. */}
+        <SubText>
+          영캠프는 대한민국 대학 불교 동아리들이 연합하여 주최하는 특별한
+          축제입니다.
+        </SubText>
+      </BannerWrapper>
     </Wrapper>
   );
 };
