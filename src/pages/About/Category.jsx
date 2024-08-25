213def181dc46b2150c2970674b57b68bd6d5639
ksinji <akstp1717@naver.com>
2024-08-25 17:21:25 +0900

    💄 style: about 페이지 데스크탑 레이아웃 구현 #22

---
+++ src/pages/About/Category.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from "./style";
+
+const Category = ({ children }) => {
+  return <S.Category>{children}</S.Category>;
+};
+
+export default Category;
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import MainTitle from "../../components/ui/MainTitle";
 import { ContentWrapper } from "../../style/commonStyle";
+import Section from "./Section";
+import { leaderProfiles, memberProfiles } from "./profileData";
+
 
 const index = () => {
   return (
-    <ContentWrapper>
-      <MainTitle title="영캠프 안내" />
-    </ContentWrapper>
+    <>
+      <MainTitle title="영캠퍼" />
+      <ContentWrapper>
+        <Section title="Team Lead" profiles={leaderProfiles} />
+        <Section title="Team Member" profiles={memberProfiles} />
+      </ContentWrapper>
+    </>
   );
 };
 
