10f15ba56bc4deee6014fc5e59f66fb254cd030f
coldweek3 <ohchanju3@naver.com>
2024-08-27 19:51:22 +0900

    :lipstick: style: ContentWrapper 스타일 변경에 따른 공지 페이지 레이아웃 조정

--- src/pages/Notification/main/index.jsx
+++ src/pages/Notification/main/index.jsx
@@ -4,10 +4,11 @@ import { ContentWrapper } from "../../../style/commonStyle";
 import styled from "styled-components";
 import Search from "./Search";
 import Content from "./Content";
+import TitleSet from "../../../components/ui/TitleSet";
 
 const Wrapper = styled.div``;
 
-const Index = () => {
+const index = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleSearch = (keyword) => {
@@ -16,8 +17,11 @@ const Index = () => {
 
   return (
     <Wrapper>
-      <MainTitle title="공지사항" />
       <ContentWrapper>
+        <TitleSet
+          mainText="공지사항"
+          subText="영캠프는 대한민국 대학 불교 동아리들이 연합하여 주최하는 특별한 축제입니다."
+        />
         <Search onSearch={handleSearch} />
         <Content keyword={searchKeyword} />
       </ContentWrapper>
@@ -25,4 +29,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default index;
