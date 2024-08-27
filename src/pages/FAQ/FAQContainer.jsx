5018f24c0d41fdee10cd9d67c48ed00bcc82b353
ksinji <akstp1717@naver.com>
2024-08-27 16:25:29 +0900

    💄 style: FAQ 페이지 반응형 레이아웃 1차 완성 #39

--- src/pages/FAQ/FAQContainer.jsx
+++ src/pages/FAQ/FAQContainer.jsx
@@ -3,9 +3,12 @@ import FAQBox from "./FAQBox";
 import * as S from "./style";
 import { ContentWrapper } from "../../style/commonStyle";
 import faqData from "../../data/faqData.json";
+import useMediaQueries from "../../hooks/useMediaQueries";
+
 
 const FAQContainer = () => {
   const [faqs, setFaqs] = useState([]);
+  const { isMobile, isTablet, isDesktop } = useMediaQueries();
 
   useEffect(() => {
     // JSON 데이터를 상태로 설정
@@ -13,13 +16,23 @@ const FAQContainer = () => {
   }, []);
 
   return (
-    <ContentWrapper>
-      <S.FAQWrapper>
-        {faqs.map((faq, index) => (
-          <FAQBox key={index} question={faq.question} answer={faq.answer} />
-        ))}
-      </S.FAQWrapper>
-    </ContentWrapper>
+    <>
+      {isDesktop ? (
+        <ContentWrapper>
+          <S.FAQWrapper isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}>
+            {faqs.map((faq, index) => (
+              <FAQBox key={index} question={faq.question} answer={faq.answer} />
+            ))}
+          </S.FAQWrapper>
+        </ContentWrapper>
+      ) : ( 
+        <S.FAQWrapper isMobile={isMobile} isTablet={isTablet} isDesktop={isDesktop}>
+          {faqs.map((faq, index) => (
+            <FAQBox key={index} question={faq.question} answer={faq.answer} />
+          ))}
+        </S.FAQWrapper>
+      )}
+    </>
   );
 };
 
