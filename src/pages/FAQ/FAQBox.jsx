39f05f4fb8dbccc709c725bdd9062c5e738c1872
ksinji <akstp1717@naver.com>
2024-08-28 02:37:50 +0900

    💄 style: 이미지 추가에 따른 수정 #39

--- src/pages/FAQ/FAQBox.jsx
+++ src/pages/FAQ/FAQBox.jsx
@@ -3,23 +3,32 @@ import * as S from "./style";
 import PropTypes from 'prop-types';
 import useMediaQueries from "../../hooks/useMediaQueries";
 
-
-import openImage from '../../assets/images/FAQ/FAQ_1.png';
-import closeImage from '../../assets/images/FAQ/FAQ_2.png';
+import openImageDesktop from '../../assets/images/FAQ/FAQ_1.png';
+import closeImageDesktop from '../../assets/images/FAQ/FAQ_2.png';
+import openImageMobile from '../../assets/images/FAQ/FAQ_3.png';
+import closeImageMobile from '../../assets/images/FAQ/FAQ_4.png';
 
 const FAQBox = ({ question, answer }) => {
-	const { isMobile } = useMediaQueries(); 
+	const { isDesktop } = useMediaQueries(); 
 	const [isOpen, setIsOpen] = useState(false); // 답변을 보여줄지 여부
+
+	const openImage = isDesktop 
+		? openImageDesktop 
+		: openImageMobile;
+
+  	const closeImage = isDesktop 
+		? closeImageDesktop 
+		: closeImageMobile; // 태블릿은 모바일 이미지와 동일함
   
 	return (
 		<S.FAQBox>
 		  <S.FAQContent>
 			<S.FAQTextContainer>
-				<S.FAQText1 isMobile={isMobile}>Q.</S.FAQText1>
-				<S.FAQText2 isMobile={isMobile}>{question}</S.FAQText2>
+				<S.FAQText1 isDesktop={isDesktop}>Q.</S.FAQText1>
+				<S.FAQText2 isDesktop={isDesktop}>{question}</S.FAQText2>
 			</S.FAQTextContainer>
-			<S.FAQButton isMobile={isMobile} onClick={() => setIsOpen(!isOpen)}>
-				<S.FAQButtonImage 
+			<S.FAQButton isDesktop={isDesktop} onClick={() => setIsOpen(!isOpen)}>
+				<img
 				src={isOpen ? closeImage : openImage} 
 				alt={isOpen ? '닫기' : '보기'} 
 				/>
@@ -28,8 +37,8 @@ const FAQBox = ({ question, answer }) => {
 		  {isOpen && (
 			<S.AnswerBox>
 			<S.FAQTextContainer>
-			<S.FAQText1 isMobile={isMobile}>A.</S.FAQText1>
-			<S.FAQText2 isMobile={isMobile}>{answer}</S.FAQText2>
+			<S.FAQText1 isDesktop={isDesktop}>A.</S.FAQText1>
+			<S.FAQText2 isDesktop={isDesktop}>{answer}</S.FAQText2>
 			</S.FAQTextContainer>
 			</S.AnswerBox>
 		  )}
