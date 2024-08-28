a77b5e62a2e965d101b30910e95912f6a3f25278
ksinji <akstp1717@naver.com>
2024-08-28 16:30:36 +0900

    📝 docs: 프로필 데이터 파일 수정 #46

--- src/data/profileData.jsx
+++ src/data/profileData.jsx
@@ -1,24 +1,38 @@
 import sampleImage from '../assets/images/About/about_sample.jpg'
 
-export const leaderProfiles = [
-	{ id: 1, name: "황진경", role: "TF장", image: sampleImage },
-	{ id: 2, name: "김준범", role: "PM", image: sampleImage },
-	{ id: 3, name: "한은서", role: "Design", image: sampleImage },
-	{ id: 4, name: "설현아", role: "Frontend", image: sampleImage },
-	{ id: 5, name: "오찬주", role: "Frontend", image: sampleImage },  
-	{ id: 6, name: "김찬빈", role: "Backend", image: sampleImage }
+export const studentCommitteeProfiles = [
+	{ id: 1, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 2, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 3, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 4, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 5, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 6, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 7, name: "김OO", role: "PM", image: sampleImage },
+	{ id: 8, name: "김OO", role: "PM", image: sampleImage }
 ];
   
-  export const memberProfiles = [
-	{ id: 1, name: "이승협", role: "PM", image: sampleImage },
-	{ id: 2, name: "이수빈", role: "Design", image: sampleImage },
-	{ id: 3, name: "윤혜정", role: "Design", image: sampleImage },
-	{ id: 4, name: "강신지", role: "Frontend", image: sampleImage },
-	{ id: 5, name: "김수빈", role: "Frontend", image: sampleImage },
-	{ id: 6, name: "백동민", role: "Frontend", image: sampleImage },
-	{ id: 7, name: "오세인", role: "Frontend", image: sampleImage },
-	{ id: 8, name: "이민수", role: "Frontend", image: sampleImage },
-	{ id: 9, name: "이유진", role: "Frontend", image: sampleImage },
-	{ id: 10, name: "김세은", role: "Backend", image: sampleImage },
-	{ id: 11, name: "임주혁", role: "Backend", image: sampleImage }
+  export const youngCamperProfiles = [
+	{ id: 1, name: "황진경", role: "TF장", image: sampleImage },
+	{ id: 2, name: "김준범", role: "PM", image: sampleImage },
+	{ id: 3, name: "이승협", role: "PM", image: sampleImage },
+
+	// 디자인
+	{ id: 4, name: "한은서", role: "디자인", image: sampleImage },
+	{ id: 5, name: "윤혜정", role: "디자인", image: sampleImage },
+	{ id: 6, name: "이수빈", role: "디자인", image: sampleImage },
+	
+	// 백엔드
+	{ id: 7, name: "김세은", role: "백엔드", image: sampleImage },
+	{ id: 8, name: "김찬빈", role: "백엔드", image: sampleImage },
+	{ id: 9, name: "임주혁", role: "백엔드", image: sampleImage },
+
+	// 프론트엔드
+	{ id: 10, name: "강신지", role: "프론트엔드", image: sampleImage },
+	{ id: 11, name: "김수빈", role: "프론트엔드", image: sampleImage },
+	{ id: 12, name: "백동민", role: "프론트엔드", image: sampleImage },
+	{ id: 13, name: "설현아", role: "프론트엔드", image: sampleImage },
+	{ id: 14, name: "오세인", role: "프론트엔드", image: sampleImage },
+	{ id: 15, name: "오찬주", role: "프론트엔드", image: sampleImage },
+	{ id: 16, name: "이민수", role: "프론트엔드", image: sampleImage },
+	{ id: 17, name: "이유진", role: "프론트엔드", image: sampleImage }
   ];
