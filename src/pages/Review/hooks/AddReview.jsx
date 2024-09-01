15d571f90abad34eb1c8e37d581bfbce96db755c
dewbeeny <dewbeeny@gmail.com>
2024-09-01 22:20:26 +0900

    ✨ feat: 사진 삭제 기능 및 비속어 필터링 기능 구현 #86

--- src/pages/Review/hooks/AddReview.jsx
+++ src/pages/Review/hooks/AddReview.jsx
@@ -1,10 +1,13 @@
-// hooks/useAddReview.js
 import { useState } from "react";
+
+//lib(비속어 필터링)
+import Filter from "badwords-ko";
 
 const useAddReview = () => {
   const [comments, setComments] = useState([]); // 기존 리뷰 데이터 관리
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const filter = new Filter(); //비속어 필터링 함수
 
   // 리뷰 데이터를 가져오는 함수
   const fetchComments = async () => {
@@ -24,13 +27,14 @@ const useAddReview = () => {
   // 새로운 리뷰를 추가하는 함수
   const addReview = async (review, password, imagePreviews) => {
     if (!review || !password) {
+      //->ReviewValidation 훅으로 대체
       alert("리뷰와 비밀번호를 입력해주세요.");
       return;
     }
 
     const newReview = {
       id: comments.length + 1,
-      text: review,
+      text: filter.clean("review"),
       date: new Date().toLocaleString(),
       image: imagePreviews[0] || null, // 첫 번째 이미지만 추가
     };
@@ -44,7 +48,7 @@ const useAddReview = () => {
       const data = await response.json();
       const updatedComments = [...data, newReview];
 
-      // 실제 파일 쓰기는 서버에서 처리되어야 합니다.
+      // 실제 파일 쓰기는 서버에서 처리
       console.log("Updated reviews:", updatedComments);
       setComments(updatedComments); // 상태 업데이트
     } catch (error) {
