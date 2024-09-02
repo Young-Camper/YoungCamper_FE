2b86a1d79a5650a9e9774238f7d0d84d3ef2e56d
이유진 <jenny7732@naver.com>
2024-09-02 19:15:29 +0900

    🐛 fix: AWS 이미지 경로 수정 #69

--- src/pages/Promotion/detail/SubTitle.jsx
+++ src/pages/Promotion/detail/SubTitle.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 import * as S from "../style";
 import clubData from "../../../data/clubData.json";
 import useMediaQueries from '../../../hooks/useMediaQueries';
 
-
-const SubTitle = ({id}) => {
+const SubTitle = ({ id }) => {
+  const mediaUrl = import.meta.env.VITE_MEDIA_URL; 
   const { isMobile, isTablet, isDesktop } = useMediaQueries();
 
   const clubId = parseInt(id, 10);
   const club = clubData.find(club => club.id === clubId);
 
-  const hasInstaLink = club.insta;
+  const hasInstaLink = !!club.insta; 
 
   return (
-    <S.Wrapper>
+    <S.Wrapper $isMobile={isMobile} $isTablet={isTablet}>
       <S.SchoolBadgeDetail>{club.school}</S.SchoolBadgeDetail>
       <S.TextAndButton>
         <S.SchoolText>{club.name}</S.SchoolText>
         {hasInstaLink && (
           <S.CTAButton $isDesktop={isDesktop} href={club.insta} target="_blank" rel="noopener noreferrer">
             <S.InstagramImage 
-              src="/src/assets/images/Promotion/insta.png"
+              src={`${mediaUrl}Promotion/insta.png`} 
               alt="Instagram"
               $isDesktop={isDesktop}
             />
@@ -29,7 +29,7 @@ const SubTitle = ({id}) => {
         )}
       </S.TextAndButton>
     </S.Wrapper>
-  )
-}
+  );
+};
 
-export default SubTitle
+export default SubTitle;
