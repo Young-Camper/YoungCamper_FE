bd0d95eecc7c547147fafdb8e9f7c69e4896ed5d
이유진 <jenny7732@naver.com>
2024-09-03 09:08:45 +0900

    🐛 fix: 뒤로가기 버튼 useNavigate 사용 #69

--- src/pages/Promotion/detail/BackPage.jsx
+++ src/pages/Promotion/detail/BackPage.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from 'react';
 import * as S from "../style";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useMediaQueries from '../../../hooks/useMediaQueries';
 
 const BackPage = () => {
     const { isMobile, isTablet, isDesktop } = useMediaQueries();
-    const mediaUrl = import.meta.env.VITE_MEDIA_URL; 
+    const mediaUrl = import.meta.env.VITE_MEDIA_URL;
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate(-1);
+    };
 
     return (
-        <Link to={`/promotion`} style={{ textDecoration: 'none' }}>
-                <S.BackContainer $isDesktop={isDesktop}>
-                    <S.BackButton>
-                        <S.ArrowIcon 
-                            $isDesktop={isDesktop} 
-                            src={`${mediaUrl}Promotion/arrowback.png`}
-                            alt="Back Arrow" 
-                        />
-                    </S.BackButton>
-                </S.BackContainer>
-            </Link>
-    )
+        <div onClick={handleBackClick} style={{ textDecoration: 'none', cursor: 'pointer' }}>
+            <S.BackContainer $isDesktop={isDesktop}>
+                <S.BackButton>
+                    <S.ArrowIcon 
+                        $isDesktop={isDesktop} 
+                        src={`${mediaUrl}Promotion/arrowback.png`}
+                        alt="Back Arrow" 
+                    />
+                </S.BackButton>
+            </S.BackContainer>
+        </div>
+    );
 }
 
-export default BackPage
+export default BackPage;
