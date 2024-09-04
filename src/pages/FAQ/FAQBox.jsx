3ffd8418aab619e8173f30ff0d2162fed22bc03f
ksinji <akstp1717@naver.com>
2024-09-04 20:50:32 +0900

    💄 style: 답변 박스에 Collapse 추가 #156

--- src/pages/FAQ/FAQBox.jsx
+++ src/pages/FAQ/FAQBox.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { Collapse } from 'reactstrap';
 import * as S from "./style";
 import PropTypes from 'prop-types';
 import useMediaQueries from "../../hooks/useMediaQueries";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const mediaUrl = import.meta.env.VITE_MEDIA_URL;
 
@@ -11,44 +13,46 @@ const openImageMobile = `${mediaUrl}FAQ/FAQ_3.png`;
 const closeImageMobile = `${mediaUrl}FAQ/FAQ_4.png`;
 
 const FAQBox = ({ question, answer, isOpen, onClick }) => {
-	const { isMobile, isTablet, isDesktop } = useMediaQueries();
+  const { isMobile } = useMediaQueries();
 
-	const openImage = isMobile
-		? openImageMobile
-		: openImageDesktop;
+  const openImage = isMobile
+    ? openImageMobile
+    : openImageDesktop;
 
-  	const closeImage = isMobile
-		? closeImageMobile 
-		: closeImageDesktop; // 태블릿은 데스크탑 이미지와 동일함
-  
-	return (
-		<S.FAQBox onClick={onClick}>
-		  <S.FAQContent>
-				<S.FAQText1 $isMobile={isMobile} >
-					<span className="a-text">Q. </span>{question}
-				</S.FAQText1>
-			<S.FAQButton $isMobile={isMobile}>
-				<img
-				src={isOpen ? closeImage : openImage} 
-				alt={isOpen ? '닫기' : '보기'} 
-				/>
-			</S.FAQButton>
-		  </S.FAQContent>
-		  {isOpen && (
-			<S.AnswerBox isOpen={isOpen}>
-			<S.FAQText2 $isMobile={isMobile} >
-				<span className="a-text">A. </span>
-				<span dangerouslySetInnerHTML={{ __html: answer }} />
-			</S.FAQText2>
-			</S.AnswerBox>
-		  )}
-		</S.FAQBox>
-	);
-  };
-  
-  FAQBox.propTypes = {
-	question: PropTypes.string.isRequired,
-	answer: PropTypes.string.isRequired,
-  };
-  
-  export default FAQBox;
+  const closeImage = isMobile
+    ? closeImageMobile 
+    : closeImageDesktop;
+
+  return (
+    <S.FAQBox onClick={onClick}>
+      <S.FAQContent>
+        <S.FAQText1 $isMobile={isMobile} >
+          <span className="a-text">Q. </span>{question}
+        </S.FAQText1>
+        <S.FAQButton $isMobile={isMobile}>
+          <img
+            src={isOpen ? closeImage : openImage} 
+            alt={isOpen ? '닫기' : '보기'} 
+          />
+        </S.FAQButton>
+      </S.FAQContent>
+      <Collapse isOpen={isOpen}>
+        <S.AnswerBox>
+          <S.FAQText2 $isMobile={isMobile} >
+            <span className="a-text">A. </span>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </S.FAQText2>
+        </S.AnswerBox>
+      </Collapse>
+    </S.FAQBox>
+  );
+};
+
+FAQBox.propTypes = {
+  question: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default FAQBox;
