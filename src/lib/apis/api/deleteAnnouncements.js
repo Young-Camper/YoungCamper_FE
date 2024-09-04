277af0153b744bc02413b45ba7a2a4bfe77cbab3
Hyeona01 <sha0209@dgu.ac.kr>
2024-09-04 18:19:16 +0900

    🐛 fix: axios delete data 수정 #20

--- src/lib/apis/api/deleteAnnouncements.js
+++ src/lib/apis/api/deleteAnnouncements.js
@@ -3,9 +3,7 @@ import { API } from "../utils";
 export const deleteAnnouncements = async (ids) => {
   try {
     const response = await API.delete(`/api/admin/`, {
-      data: { 
         ids: ids,
-      },
     });
     console.log("API success: ", response);
     return response.data;
