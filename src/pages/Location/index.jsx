cc739489cd1f88cbf6f648272b81cf0d4fd9ed72
Hyeona01 <sha0209@dgu.ac.kr>
2024-09-05 17:49:25 +0900

    📝 docs: 유의사항 실제데이터 반영 #107

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -3,11 +3,9 @@ import MainTitle from "../../components/ui/MainTitle";
 import { ContentWrapper } from "../../style/commonStyle";
 import Attention from "../../components/ui/Attention";
 import LocMap from "./LocMap";
-import Transport from "./transport"
+import Transport from "./transport";
 
 import useMediaQueries from "../../hooks/useMediaQueries";
-
-
 
 const index = () => {
   const { isMobile, isTablet, isDesktop } = useMediaQueries();
@@ -18,21 +16,29 @@ const index = () => {
         mainText="장소안내"
         subText="장충체육관에서 여러분을 기다립니다."
       />
-      
-      { isDesktop ? (
-      <ContentWrapper>
-        <Transport />
-        <LocMap />
-        <Attention />
-      </ContentWrapper>
-  ) : (
-    <>
-      <Transport />
-      <LocMap />
-      <Attention />
+
+      {isDesktop ? (
+        <ContentWrapper>
+          <Transport />
+          <LocMap />
+          <Attention
+            attention1="∙ 화장실은 2층에 위치해있으며, 사용 시 대기시간이 발생할 수 있습니다."
+            attention2="∙ 방문 시 안전을 위해 현장 통제를 따라주시길 바랍니다."
+            attention3="∙ 층 간 이동은 불가합니다."
+          />
+        </ContentWrapper>
+      ) : (
+        <>
+          <Transport />
+          <LocMap />
+          <Attention
+            attention1="∙ 화장실은 2층에 위치해있으며, 사용 시 대기시간이 발생할 수 있습니다."
+            attention2="∙ 방문 시 안전을 위해 현장 통제를 따라주시길 바랍니다."
+            attention3="∙ 층 간 이동은 불가합니다."
+          />
+        </>
+      )}
     </>
-  )}
-  </>
   );
 };
 
