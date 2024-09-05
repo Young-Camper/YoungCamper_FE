7021cafb11d95b057d2de3d4b79dcb37c1790b63
coldweek3 <ohchanju3@naver.com>
2024-09-06 02:10:57 +0900

    :sparkles: notice, faq, about 다국어 적용

--- src/pages/Intro/IntroBanner.jsx
+++ src/pages/Intro/IntroBanner.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import * as S from "./style";
 import useMediaQueries from "../../hooks/useMediaQueries";
+import { useTranslation } from "react-i18next";
 
 const IntroBanner = () => {
   const { isDesktop } = useMediaQueries();
   const mediaUrl = import.meta.env.VITE_MEDIA_URL;
+  const { t } = useTranslation();
 
   return (
     <S.IntroBanner $isDesktop={isDesktop}>
       <S.IntroImg src={`${mediaUrl}Intro/intro_banner.png`} />
       <S.IntroWrapper $isDesktop={isDesktop}>
         <S.IntroTitle $isDesktop={isDesktop}>
-          대한민국 최대 규모 <br />
-          청년 불자의 축제,
+          {t(`about.bannerSubtitle`)}
         </S.IntroTitle>
         <S.IntroLogo $isDesktop={isDesktop}>YOUNG CAMP</S.IntroLogo>
       </S.IntroWrapper>
