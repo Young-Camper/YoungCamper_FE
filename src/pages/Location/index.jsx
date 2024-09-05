6e1c417115181dbb6e14af8e7a219f909b02d006
Hyeona01 <sha0209@dgu.ac.kr>
2024-09-05 18:12:08 +0900

    💄 style: 장소안내 페이지 유의사항 업데이트 #151

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -3,7 +3,6 @@ import MainTitle from "../../components/ui/MainTitle";
 import Attention from "../../components/ui/Attention";
 import LocMap from "./LocMap";
 import Transport from "./transport";
-import Transport from "./transport";
 
 import * as S from "./style";
 
@@ -18,20 +17,19 @@ const index = () => {
         mainText="장소안내"
         subText="장충체육관에서 여러분을 기다립니다."
       />
-
-      {isDesktop ? (
-        <ContentWrapper>
-          <Transport />
-          <LocMap />
-          <Attention />
-        </ContentWrapper>
-      ) : (
-        <>
-          <Transport />
-          <LocMap />
-          <Attention />
-        </>
-      )}
+      <S.Wrapper
+        $isMobile={isMobile}
+        $isTablet={isTablet}
+        $isDesktop={isDesktop}
+      >
+        <Transport />
+        <LocMap />
+        <Attention
+          attention1="∙ 화장실은 2층에 위치해있으며, 사용 시 대기시간이 발생할 수 있습니다."
+          attention2="∙ 방문 시 안전을 위해 현장 통제를 따라주시길 바랍니다."
+          attention3="∙ 층 간 이동은 불가합니다."
+        />
+      </S.Wrapper>
     </>
   );
 };
