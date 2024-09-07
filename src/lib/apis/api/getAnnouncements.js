42cfa2bcadad12d36bba6b6f4938dcf8f7fb6788
coldweek3 <ohchanju3@naver.com>
2024-09-08 03:39:51 +0900

    :sparkles: api 변경에 따른 공지 api 수정 - 홈, 공지 main, detail, search

--- src/lib/apis/api/getAnnouncements.js
+++ src/lib/apis/api/getAnnouncements.js
@@ -1,9 +1,13 @@
 import { API } from "../utils";
 
-export const getAnnouncements = async () => {
+export const getAnnouncements = async (languageCode) => {
   try {
-    const response = await API.get("/api/announcements");
-    // console.log(response.data);
+    const response = await API.get("/api/announcements", {
+      headers: {
+        "Accept-Language": languageCode,
+      },
+    });
+    // console.log(response);
     return response;
   } catch (error) {
     // console.error("API error: ", error);
