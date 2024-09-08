4fe283c4e1e02278669d6c86edc44e42aa4021e6
Hyeona01 <sha0209@dgu.ac.kr>
2024-09-08 14:00:37 +0900

    🐛 fix: 모달 열렸을 때 스크롤 막기

--- src/pages/Performance/index.jsx
+++ src/pages/Performance/index.jsx
@@ -1,26 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MainTitle from "../../components/ui/MainTitle";
 import { ContentWrapper } from "../../style/commonStyle";
 import TimeTable from "./TimeTable";
 import Attention from "../../components/ui/Attention";
 import useMediaQueries from "../../hooks/useMediaQueries";
-import ArtistModal from "./ArtistModal"; // 모달을 독립적으로 관리
+import ArtistModal from "./ArtistModal";
+import { useTranslation } from "react-i18next";
 
-const Index = () => {
+const index = () => {
   const { isMobile, isTablet, isDesktop } = useMediaQueries();
   const [modalOpen, setModalOpen] = useState(false);
   const [clickedArtist, setClickedArtist] = useState(null);
+  const { t, i18n } = useTranslation();
+
+  const currentLanguage = i18n.language;
+
+  const Gap = () => {
+    if (isMobile) {
+      return currentLanguage === "en" ? "0px" : "7px";
+    }
+    return "24px";
+  };
+
+  const caution = t("caution.caution", { returnObjects: true });
 
   const handleOpenModal = (artist) => {
     setClickedArtist(artist);
     setModalOpen(true);
   };
 
+  // 모달 열렸을 때 스크롤 막기
+  useEffect(() => {
+    if (modalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [modalOpen]);
+
   return (
     <>
       <MainTitle
-        mainText="공연안내"
-        subText="이번 영캠프에서는 특별한 무대들이 준비되어 있어요!"
+        mainText={t("schedule.bannerTitle")}
+        subText={t("schedule.subtitle")}
+        gap={Gap}
       />
       <ContentWrapper
         $isMobile={isMobile}
@@ -30,9 +57,13 @@ const Index = () => {
         <TimeTable
           modalOpen={modalOpen}
           setModalOpen={setModalOpen}
-          onArtistClick={handleOpenModal} // 모달 열기 핸들러 전달
+          onArtistClick={handleOpenModal}
         />
-        <Attention />
+        <Attention
+          attention1={caution[0]}
+          attention2={caution[1]}
+          attention3={caution[2]}
+        />
       </ContentWrapper>
       {modalOpen && (
         <ArtistModal artist={clickedArtist} setModalOpen={setModalOpen} />
@@ -41,4 +72,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default index;
