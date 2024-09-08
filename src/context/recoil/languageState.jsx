d0997c9a71631f33a99879573993c6662784406a
ksinji <akstp1717@naver.com>
2024-09-08 15:11:17 +0900

    🐛 fix: 언어 상태 유지 되도록 수정

--- src/context/recoil/languageState.jsx
+++ src/context/recoil/languageState.jsx
@@ -3,4 +3,11 @@ import { atom } from "recoil";
 export const languageState = atom({
   key: "languageState",
   default: localStorage.getItem("language") || "ko",
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet((nowLang) => {
+        localStorage.setItem("language", nowLang); // 상태가 변경될 때 localStorage에 저장
+      });
+    },
+  ],
 });
