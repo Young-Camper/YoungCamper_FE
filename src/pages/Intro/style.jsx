32c52ce5e9094bfbc361a9be56f62ff0500e5f0b
Hyeona01 <sha0209@dgu.ac.kr>
2024-09-19 21:14:31 +0900

    📝 docs: 동아리 데이터, FAQ 데이터 업데이트 #151

--- src/pages/Intro/style.jsx
+++ src/pages/Intro/style.jsx
@@ -139,4 +139,17 @@ export const JoinBtn = styled.div`
   font-style: normal;
   line-height: ${(props) => (props.$isDesktop ? "28px" : "18px")};
   letter-spacing: ${(props) => (props.$isDesktop ? "-0.2px" : "-0.12px")};
+
+  border: 1px solid #000;
+  background: var(--Base-Real-White, #fff);
+  transition: 0.3s ease;
+  color: #000;
+  &:hover {
+    background: var(
+      --gradient,
+      linear-gradient(102deg, #0068ff 23.99%, #b9ff9c 98.08%)
+    );
+    border: none;
+    color: #fff;
+  }
 `;
